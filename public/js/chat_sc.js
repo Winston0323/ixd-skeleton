89157ba40758c58637a9bc463e07243271a014d3
Huiran Song <husong@ucsd.edu>
2021-03-12 13:33:10 -0800

    omw to test jquery post on chat; gal gone risk it all :))

--- public/js/chat_sc.js
+++ public/js/chat_sc.js
@@ -11,6 +11,25 @@ $(document).ready(function () {
 function initializePage() {
     console.log("path = " + location.pathname);
     var path = location.pathname;
+    $("#snedTextForm").submit(function (event) {
+
+        // Stop form from submitting normally
+        event.preventDefault();
+
+        // Get some values from elements on the page:
+        var $form = $(this),
+            term = $form.find("input[name='s']").val(),
+            url = $form.attr("action");
+
+        // Send the data using post
+        var posting = $.post(url, { s: term });
+
+        // Put the results in a div
+        posting.done(function (data) {
+            var content = $(data).find("#content");
+            $("#result").empty().append(content);
+        });
+    });
     $('.btn.status').click(function () {
         location.href = "http://localhost:3000" + path + "?text=What%27s+the+status+of+the+order%3F";
     });
