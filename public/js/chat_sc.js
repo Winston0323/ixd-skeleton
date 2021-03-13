6a6e6d50ef836e09ed99a060a56294415113dcbc
Winston0323 <liuzixi0323@gmail.com>
2021-03-12 21:05:58 -0800

    fixed chat-sc

--- public/js/chat_sc.js
+++ public/js/chat_sc.js
@@ -18,7 +18,7 @@ function initializePage() {
         var $form = $(this),
             term = $form.find("input[name='txt']").val(),
             // url = $form.attr("action");
-            url = "http://localhost:3000" + path;
+            url = "https://a8-artcommissioner.herokuapp.com/" + path;
         // Send the data using post
         var posting = $.post(url, { txt: term, p: url });
 
@@ -32,7 +32,7 @@ function initializePage() {
         // Get some values from elements on the page:
         var term = "I would like to know my order status",
             // url = $form.attr("action");
-            url = "http://localhost:3000" + path;
+            url = "https://a8-artcommissioner.herokuapp.com/" + path;
         var posting = $.post(url, { txt: term, p: url });
 
         posting.done(function (data) {
@@ -45,7 +45,7 @@ function initializePage() {
         // Get some values from elements on the page:
         var term = "I'm looking to commission a piece of work. Do you have any open commission spot?",
             // url = $form.attr("action");
-            url = "http://localhost:3000" + path;
+            url = "https://a8-artcommissioner.herokuapp.com/" + path;
         var posting = $.post(url, { txt: term, p: url });
 
         posting.done(function (data) {
@@ -58,7 +58,7 @@ function initializePage() {
         // Get some values from elements on the page:
         var term = "I'd like to cancel my order.",
             // url = $form.attr("action");
-            url = "http://localhost:3000" + path;
+            url = "https://a8-artcommissioner.herokuapp.com/" + path;
         var posting = $.post(url, { txt: term, p: url });
 
         posting.done(function (data) {
@@ -71,7 +71,7 @@ function initializePage() {
         // Get some values from elements on the page:
         var term = "Here's the deposit.",
             // url = $form.attr("action");
-            url = "http://localhost:3000" + path;
+            url = "https://a8-artcommissioner.herokuapp.com/" + path;
         var posting = $.post(url, { txt: term, p: url });
 
         posting.done(function (data) {
@@ -79,6 +79,5 @@ function initializePage() {
             // var content = $(data).find("#content");
         });
     });
-
     // $('#colorBtn').click(randomizeColors);
 }
